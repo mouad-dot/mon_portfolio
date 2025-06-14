6259e06d36c6a80154e714e6a0564c53be51da19
Mouad MAAFI <mouadmaafi1@gmail.com>
2025-06-14 21:46:49 +0100

    Ajoute Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
